58be35b0eb8d74c1fe66be3f68e2950c7e671dc0
nd823 <nandong823@gmail.com>
2019-10-22 11:53:25 -0400

    further reduce map data size

--- app.py
+++ app.py
@@ -357,6 +357,8 @@ FONT_AWESOME = "https://use.fontawesome.com/releases/v5.10.2/css/all.css"
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.MINTY,  FONT_AWESOME])
 
+server = app.server
+
 tabs = dbc.Tabs(
     [
         dbc.Tab(tab1_content, label="KPIs",
@@ -600,4 +602,4 @@ def update_figure(X):
     ]
 
 if __name__ == "__main__":
-    app.run_server(host='127.0.0.1', port='9000')
+    app.run_server(debug=True)
